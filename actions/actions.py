e7559adc242a6ba4994cfa40e9c28e75fb51de9b
neil <neil@gmail.com>
2025-01-17 13:19:35 -0500

    cambios-responder-5-minutos

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,78 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import ReminderScheduled, EventType
+from rasa_sdk.events import ReminderCancelled
+from datetime import datetime, timedelta
+import requests
+
+class ActionSetReminder(Action):
+    def name(self) -> str:
+        return "action_set_reminder"
+
+    async def run(self, dispatcher: CollectingDispatcher, tracker, domain) -> list[EventType]:
+        # Configura el recordatorio para 5 minutos en el futuro
+        events = [ReminderCancelled()]
+
+        reminder_time = datetime.now() + timedelta(minutes=2)
+        entities = tracker.latest_message.get("entities")
+
+        reminder = ReminderScheduled(
+            "EXTERNAL_reminder",
+            trigger_date_time=reminder_time,
+            entities=entities,
+            name="delayed_message_reminder",    
+            kill_on_user_message=False  # Cambiado a False para que el mensaje se envíe incluso si el usuario escribe
+        )
+
+        # Mensaje inmediato
+        events.append(reminder)
+        dispatcher.utter_message(text="¡Hola! Gracias por tu mensaje.")
+        return [reminder]
+
+class ActionReactToReminder(Action):
+    def name(self) -> str:
+        return "action_react_to_reminder"
+
+    async def run(self, dispatcher: CollectingDispatcher, tracker, domain) -> list[EventType]:
+        # Obtener el ID del remitente
+        sender_id = tracker.sender_id
+        
+        # Obtener metadatos si están disponibles
+        metadata = tracker.get_slot("session_started_metadata") or {}
+        
+        # Datos que enviaremos en el POST
+        data = {
+            "message": "👩‍💻 ¡Hola! ¿Sigues ahí? Si necesitas más información, no dudes en llamarnos al 📞 (01) 641 4300. Estaremos encantados de ayudarte.",
+            "sender_id": sender_id,
+        }
+
+        try:
+            response = requests.post('http://localhost:3000/api/v1/whatsapp', json=data)
+            if response.status_code == 200:
+                dispatcher.utter_message(text=f"¡Hola de nuevo {sender_id}! Han pasado 5 minutos desde tu último mensaje.")
+            else:
+                print(f"Error en la llamada al endpoint: {response.status_code}")
+                print(f"Sender ID: {sender_id}")
+                print(f"Metadata: {metadata}")
+        except Exception as e:
+            print(f"Error al hacer el POST: {str(e)}")
+
+        return []
+
+# class ForgetReminders(Action):
+
+#     def name(self) -> Text:
+#         return "action_forget_reminders"
+
+#     async def run(
+#         self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+#     ) -> List[Dict[Text, Any]]:
+
+#         dispatcher.utter_message(f"Okay, I'll cancel all your reminders.")
+
+#         # Cancel all reminders
+#         return [ReminderCancelled()]
